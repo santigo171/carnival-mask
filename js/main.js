bd8b0a69e741c982ba3070ae649a40d33c975831
David Hurtado <santibetancourt17@gmail.com>
2021-05-28 21:16:28 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function radian(degrees) {
+  return (Math.PI / 180) * degrees;
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Brazilian Carnival Game
+
+By **La Qualité** 9A:
+
+- Jose Moreno
+- Santiago Gil
+- Yum Sabogal
+- David Hurtado
+
+![logo](https://i.postimg.cc/htP2bVWr/la-qualite.jpg "logo")
